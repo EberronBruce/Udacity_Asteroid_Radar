11e96dfde6e81f239bd704f234321e8bf5ea2344
Bruce <bruce@redravencomputing.com>
2021-04-04 03:18:29 -0600

    Refractor

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,10 +4,9 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "CHII60uETgFUbqsFXMCTjx3KxpzYdaFpaUPHe51Z"
+    const val API_KEY = "Put Your API KEY here"
     const val IMAGE_KEY = "image"
     const val START_DATE_QUERY = "start_date"
-    const val END_DATE_QUERY = "end_date"
     const val API_KEY_QUERY = "api_key"
     const val NUMBER_OF_DAYS_IN_WEEK = 8
 }
